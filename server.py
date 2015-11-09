2b82528f62c93f7c53c4a87c4724c3ce91bffd60
Kelin Christi <christi.kelin20@gmail.com>
2015-11-09 14:14:37 -0800

    Added code in server.py and index.html and I'm about to test it

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, request, session, flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+app = Flask(__name__)
+app.secret_key = 'tiredofsecrets'
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/form', methods=['POST'])
+def form():
+	session ['e-mail'] = request.form ['e-mail']
+	session ['first_name'] = request.form ['first_name']
+	session ['last_name'] = request.form ['last_name']
+	session ['password'] = request.form ['password']
+	session ['pwordconfirm'] = request.form ['pwordconfirm']
+
+	if len(request.form['e-mail']) < 1
+	or len(request.form['first_name']) < 1 
+	or len(request.form['last_name']) < 1 
+	or len(request.form['password']) < 1 
+	or len(request.form['pwordconfirm']) < 1:
+		flash('Please fill out all fields!')
+	elif not EMAIL_REGEX.match(request.form['e-mail']):
+		flash('e-mail address not valid!')
+	elif len(request.form['password']) < 8
+	or len(request.form['pwordconfirm']) < 8:
+		flash('Passwords must match and be atleast 8 characters long.')
+	elif request.form['password'] != request.form['pwordconfirm']:
+		flash('Passwords do not match, please enter again!')
+	else:
+		flash('Thanks for submitting your information!')
+	return redirect('/')
+
+app.run(debug=True)
